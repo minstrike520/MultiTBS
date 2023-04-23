88a9113bc1e334c52ee3276ebb236e3d996bf7a0
minstrike520 <minstrike520@gmail.com>
2023-04-23 18:50:24 +0800

    Tidies up create.js

--- scenes/GameStage/createCallback/create.js
+++ scenes/GameStage/createCallback/create.js
@@ -6,19 +6,23 @@ export default function (game)
 {
     tileMap.load(game);
     
-    game.cameras.main.setZoom(2);
-    
-    game.cameras.main.setScroll(game.cameras.main.displayWidth/2-400,game.cameras.main.displayHeight/2-300);
+    game.cameras.main.setZoom(2).setOrigin(0);
 
-    game.txt = game.add.text(game.cameras.main.displayWidth,game.cameras.main.displayHeight, "Loading...", {font: "25px Arial", fill: "yellow"}).setScale(0.8).setOrigin(1,1);
+    game.txt = game.add.text
+    (
+        game.cameras.main.displayWidth,
+        
+        game.cameras.main.displayHeight, 
+        
+        "Loading...", 
+        
+        {font: "25px Arial", fill: "yellow"}
+        
+    ).setScale(0.8).setOrigin(1,1);
 
     game.rect1 = game.add.rectangle(0,0,30,30, "blue").setOrigin(0);
 
     game.rect2 = game.add.rectangle(game.cameras.main.displayWidth,game.cameras.main.displayHeight,30,30, "blue").setOrigin(1);
-
-    game.cameras.main.setOrigin(0);
-
-    game.cameras.main.setScroll(0,0);
 
     game.inGameUI = game.add.container(0,0, [
 
