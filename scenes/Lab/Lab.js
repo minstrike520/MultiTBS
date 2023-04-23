8e02ce287bb8b03678336b5d47e914ba9dbd51aa
minstrike520 <minstrike520@gmail.com>
2023-04-23 22:18:05 +0800

    Adds some testing object in Lab Scene

--- scenes/Lab/Lab.js
+++ scenes/Lab/Lab.js
@@ -1,14 +1,17 @@
+import create from "./createCallback/create.js";
+
 export default class Lab extends Phaser.Scene {
     constructor() {
         super("lab");
+        document.gameLab = this;
     }
 
     preload() {
-        
+        this.load.image('background', "assets/YamaBackground.png")
     }
 
     create() {
-        
+        create(this);
     }
 
     update() {
