4028cec859f000191254d3fd26c0c60905364eba
minstrike520 <minstrike520@gmail.com>
2023-04-26 16:44:03 +0800

    Set Connection with server through socket.io

--- scenes/StartScene/StartScene.js
+++ scenes/StartScene/StartScene.js
@@ -1,12 +1,18 @@
 import addButton from "../tools/addButton.js";
 
+let $ = io.connect("http://localhost:80/");
+
 export default class StartScene extends Phaser.Scene {
     constructor() {
         super("custom-button")
     }
 
     preload() {
-        
+        $.emit("preload", "preload complete");
+
+        $.on("_connected", function(e){
+            console.log(e)
+        })
     }
 
     create() 
@@ -35,6 +41,46 @@ export default class StartScene extends Phaser.Scene {
         {
             game.scene.start("lab");
         })
+
+        let SendInput = new CustomEvent("send-input")
+
+        let inp = document.createElement("input");
+
+        inp.className="1";
+
+        inp.onfocus = function () {
+
+            inp.keydown = this.addEventListener("keydown", function(e) {
+                
+                if (e.key === "Enter") {
+                    console.log("Input enter", e.key)
+                }                
+            })
+        }
+        inp.onblur = function () {
+
+            console.log("blurred")
+
+            this.removeEventListener("keydown", this.keydown);
+        }
+        
+
+        let serverUrlInput = game.add.dom(
+            0,0,
+            DOMContainer.appendChild(
+                inp
+            ),
+            `   background-color: yellow;
+                width: 100px;
+                height: 100px;
+                placeholder: Input server url
+            `,
+        )
+        .addListener('input')
+        .on("input", function (e) {
+            console.log(e.target.value);
+        })
+        .setOrigin(0)
         
 
 
